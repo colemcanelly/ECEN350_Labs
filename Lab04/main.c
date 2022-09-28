f3088ffbe9cfb3c76c2224f47262c5a6fdbc2069
Cole McAnelly <cole.mcanelly3@gmail.com>
2022-09-28 20:31:48 +0100

    Finished lab4

--- Lab04/main.c
+++ Lab04/main.c
@@ -3,10 +3,14 @@
 #include <stdio.h>
 
 extern long long int my_mul(long long int a, long long int b);
+extern long long int fact(long long int n);
 
 int main(void)
 {
     long long int a = my_mul(3, 5);
     printf("Result of my_mul(3, 5) = %lld\n", a);
+    long long int n = 3;
+    long long int b = fact(n);
+    printf("Result of fact(%lld) = %lld\n", n, b);
     return 0;
 }
