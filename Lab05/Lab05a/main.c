2b9793363e3566d5c9d6dc7802636185402b5945
Cole McAnelly <cole.mcanelly3@gmail.com>
2022-10-12 20:00:28 +0100

    Finished lab05

--- Lab05/Lab05a/main.c
+++ Lab05/Lab05a/main.c
@@ -3,12 +3,13 @@
 #include <stdio.h>
 
 extern long long int test(long long int a, long long int b);
-// extern long long int test2(long long int d, long long int e);
+extern long long int test2(long long int d, long long int e);
 
 int main(void)
 {
     long long int c = test(3, 5);
     printf("Result of test(3, 5) = %lld\n", c);
-//    long long int f = test2(6, 5);
+    long long int f = test2(6, 5);
+    // printf("Result of test(6, 5) = %lld\n", f);
     return 0;
 }
