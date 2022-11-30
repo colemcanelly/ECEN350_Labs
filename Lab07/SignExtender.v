ac3fc579591a32b20416a223476d754917507a0d
Cole McAnelly <cole.mcanelly3@gmail.com>
2022-11-30 12:51:04 -0600

    Modified SignExtender to support MOVZ instruction for Lab09 Q5

--- Lab07/SignExtender.v
+++ Lab07/SignExtender.v
@@ -1,27 +1,34 @@
-`define I    2'b00
-`define D    2'b01
-`define B    2'b10
-`define CB   2'b11
+`define I   3'b000
+`define D   3'b001
+`define B   3'b010
+`define CB  3'b011
+`define IM  3'b100
 
 module SignExtender(BusImm, Input, Ctrl); 
    output [63:0] BusImm; 
    input [25:0]  Input; 
-   input [1:0]	 Ctrl; 
+   input [2:0]	 Ctrl; 
    
    wire 	 extBit;
 
-   // Passed Tests
+   wire [6:0] 	 shift;
+   wire [63:0]   movz;
+
+   assign shift = Input[22:21] << 4;     // Get shift * 16
+   assign movz  = Input[20:5] << shift;  // movz_imm << shift
+
    assign extBit =
-		  (Ctrl == `I) ? Input[21] : // I
-		  (Ctrl == `D) ? Input[20] : // D
-		  (Ctrl == `B) ? Input[25] : // B
-                      Input[23];             // CB
+		  (Ctrl ==  `I) ? Input[21] : // I
+		  (Ctrl ==  `D) ? Input[20] : // D
+		  (Ctrl ==  `B) ? Input[25] : // B
+          (Ctrl == `CB) ? Input[23] : // CB
+          1'b0;                       // IM and default
 
    
    assign BusImm =
-		  (Ctrl == `I) ? {{52{extBit}}, Input[21:10]} : // I
-		  (Ctrl == `D) ? {{55{extBit}}, Input[20:12]} : // D
-		  (Ctrl == `B) ? {{38{extBit}}, Input} :        // B
-		      {{45{extBit}}, Input[23:5]};              // CB
-   
+		  (Ctrl ==  `I) ? {{52{extBit}}, Input[21:10]} : // I
+		  (Ctrl ==  `D) ? {{55{extBit}}, Input[20:12]} : // D
+		  (Ctrl ==  `B) ? {{38{extBit}}, Input[25: 0]} : // B
+          (Ctrl == `CB) ? {{45{extBit}}, Input[23: 5]} : // CB
+          (Ctrl == `IM) ? {movz} : {64'b0};  // Default BusImm = 0
 endmodule
--- Lab07/SignExtenderTest.v
+++ Lab07/SignExtenderTest.v
@@ -34,8 +34,8 @@ module SignExtenderTest_v;
 		input [`STRLEN*8:0] testType;
 		inout [7:0] passed;
 	
-		if(actualOut == expectedOut) begin $display ("%s passed", testType); passed = passed + 1; end
-		else $display ("%s failed: %x should be %x", testType, actualOut, expectedOut);
+		if(actualOut == expectedOut) begin $display ("\tpassed\t%s", testType); passed = passed + 1; end
+		else $display ("\tfailed\t%s\n\t: %x should be %x", testType, actualOut, expectedOut);
 	endtask
 	
 	task allPassed;
@@ -48,7 +48,7 @@ module SignExtenderTest_v;
 
 	// Inputs
         reg [25:0]  Input;
-        reg [1:0]  Ctrl;
+        reg [2:0]  Ctrl;
 	reg [7:0] passed;
 
 	// Outputs
@@ -69,20 +69,34 @@ module SignExtenderTest_v;
 
                 // Here is one example test vector, testing the ADD instruction:
 	        $display ("8 Test Vectors:");
-		{Input, Ctrl} = {26'h444, 2'h0}; #40; passTest({BusImm}, 64'h1, "I  :: Positive", passed);
-	        {Input, Ctrl} = {26'hfff73ff, 2'h0}; #40; passTest({BusImm}, 64'hffffffffffffffdc, "I  :: Negative", passed);
-	        {Input, Ctrl} = {26'h1234, 2'h1}; #40; passTest({BusImm}, 64'h1, "D  :: Positive", passed);
-	        {Input, Ctrl} = {26'hffdcfff, 2'h1}; #40; passTest({BusImm}, 64'hffffffffffffffdc, "D  :: Negative", passed);
-	        {Input, Ctrl} = {26'h24, 2'h2}; #40; passTest({BusImm}, 64'h24, "B  :: Positive", passed);
-	        {Input, Ctrl} = {26'hfffffdc, 2'h2}; #40; passTest({BusImm}, 64'hffffffffffffffdc, "B  :: Negative", passed);
-	        {Input, Ctrl} = {26'h20, 2'h3}; #40; passTest({BusImm}, 64'h1, "CB :: Positive", passed);
-	        {Input, Ctrl} = {26'hffffb9f, 2'h3}; #40; passTest({BusImm}, 64'hffffffffffffffdc, "CB :: Negative", passed);
+			{Input, Ctrl} = {26'h444, 3'h0}; #40; passTest({BusImm}, 64'h1, "I  :: Positive", passed);
+	        {Input, Ctrl} = {26'hfff73ff, 3'h0}; #40; passTest({BusImm}, 64'hffffffffffffffdc, "I  :: Negative", passed);
+	        {Input, Ctrl} = {26'h1234, 3'h1}; #40; passTest({BusImm}, 64'h1, "D  :: Positive", passed);
+	        {Input, Ctrl} = {26'hffdcfff, 3'h1}; #40; passTest({BusImm}, 64'hffffffffffffffdc, "D  :: Negative", passed);
+	        {Input, Ctrl} = {26'h24, 3'h2}; #40; passTest({BusImm}, 64'h24, "B  :: Positive", passed);
+	        {Input, Ctrl} = {26'hfffffdc, 3'h2}; #40; passTest({BusImm}, 64'hffffffffffffffdc, "B  :: Negative", passed);
+	        {Input, Ctrl} = {26'h20, 3'h3}; #40; passTest({BusImm}, 64'h1, "CB :: Positive", passed);
+	        {Input, Ctrl} = {26'hffffb9f, 3'h3}; #40; passTest({BusImm}, 64'hffffffffffffffdc, "CB :: Negative", passed);
+			// Tests for new operation in Lab09: MOVZ
+			{Input, Ctrl} = {26'h399c62f, 3'h4}; #40; passTest({BusImm}, 64'hce31, "IM :: Test #1 [ce31 << 0]", passed);
+			{Input, Ctrl} = {26'h3b9c62f, 3'h4}; #40; passTest({BusImm}, 64'hce310000, "IM :: Test #2 [ce31 << 16]", passed);
+			{Input, Ctrl} = {26'h3d9c62f, 3'h4}; #40; passTest({BusImm}, 64'hce3100000000, "IM :: Test #3 [ce31 << 32]", passed);
+			{Input, Ctrl} = {26'h3f9c62f, 3'h4}; #40; passTest({BusImm}, 64'hce31000000000000, "IM :: Test #4 [ce31 << 48]", passed);
+			{Input, Ctrl} = {26'h101418c, 3'h4}; #40; passTest({BusImm}, 64'ha0c, "IM :: Test #5 [a0c << 0]", passed);
+			{Input, Ctrl} = {26'h121418c, 3'h4}; #40; passTest({BusImm}, 64'ha0c0000, "IM :: Test #6 [a0c << 16]", passed);
+			{Input, Ctrl} = {26'h141418c, 3'h4}; #40; passTest({BusImm}, 64'ha0c00000000, "IM :: Test #7 [a0c << 32]", passed);
+			{Input, Ctrl} = {26'h161418c, 3'h4}; #40; passTest({BusImm}, 64'ha0c000000000000, "IM :: Test #8 [a0c << 48]", passed);
+			{Input, Ctrl} = {26'h305c7b9, 3'h4}; #40; passTest({BusImm}, 64'h2e3d, "IM :: Test #9 [2e3d << 0]", passed);
+			{Input, Ctrl} = {26'h325c7b9, 3'h4}; #40; passTest({BusImm}, 64'h2e3d0000, "IM :: Test #10 [2e3d << 16]", passed);
+			{Input, Ctrl} = {26'h345c7b9, 3'h4}; #40; passTest({BusImm}, 64'h2e3d00000000, "IM :: Test #11 [2e3d << 32]", passed);
+			{Input, Ctrl} = {26'h365c7b9, 3'h4}; #40; passTest({BusImm}, 64'h2e3d000000000000, "IM :: Test #12 [2e3d << 48]", passed);
 	        
 		//Reformate and add your test vectors from the prelab here following the example of the testvector above.	
 
 
-		allPassed(passed, 8);
-	end
+		allPassed(passed, 20);
+	end  
+
       
 endmodule
 
